1568813d99a8903870c2c274eddd11f33118c1b5
Joao Fuzer <fuuzer_@hotmail.com>
2025-10-13 23:50:35 -0300

    first commit hahahahaha

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react"
+import Header from "./components/Header/Header"
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <main className="container">
+        <div className="card">
+          <h1>Fuzer DEV</h1>
+          <p>Hi! My name is João,  I'm 23 years old and I'm from Brazil. Let's built more than a website a experience!</p>
+        </div>
+        <section className="projects-section">
+          <h2>Projects(2)</h2>
+          <div className="projects-cards">
+            <div className="card"></div>
+          </div>
+        </section>
+      </main>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header className='header-container'>
+      <nav className='header-nav'>
+        <h1>JF</h1>
+        <ul>
+          <li>Projects</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
+
+export default Header
