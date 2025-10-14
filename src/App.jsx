49a71f84573b8e35f2f30e469ef6ef38906709ba
Joao Fuzer <fuuzer_@hotmail.com>
2025-10-14 00:27:41 -0300

    message construct

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ function App() {
     <>
       <Header />
       <main className="container">
+        <div className="in-progress-container">
+          <h1>🚧 Website Under Construction 🚧</h1>
+        </div>
         <div className="card">
           <h1>Fuzer DEV</h1>
           <p>Hi! My name is João,  I'm 23 years old and I'm from Brazil. Let's built more than a website a experience!</p>
         </div>
         <section className="projects-section">
-          <h2>Projects(2)</h2>
+          <h2>Projects(?)</h2>
           <div className="projects-cards">
             <div className="card"></div>
           </div>
